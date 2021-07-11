a47c4cc86a337569ca15020a6e278343393b8990
Valentin_Tsushko <35625439+ValentinTsushko@users.noreply.github.com>
2021-07-11 08:30:56 +0300

    Create pirson.h

---
+++ Generator random numb. and check the Pearson crit/pirson.h
@@ -0,0 +1,22 @@
+// GNU nano 4.8                        pirson.h
+
+#include "var_1.h"
+
+//class Pirson : public Var
+class Pirson
+{
+public:
+        //int sort(double *n);
+        double sample(double * a);
+        double dispersion(double * a, double x_b);
+        double a(double x_b, double delta);
+        double b(double x_b, double delta);
+ // take probability of hitting x between x_i and x_i+1
+        double * P_i(double * a, double * P_i, double b_a);  // b_a = b - a
+        double ksi_observation(double * a, double * P_i);
+
+        bool Run(double * a);
+};
+
+
+
